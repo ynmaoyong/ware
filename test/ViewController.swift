ceded0c40208b777797045a05f353084c53312b1
云南省昆明市Mao <mac@yunnanshengkunmingshimaodemacbook-pro.local>
2015-12-03 19:24:44 +0800

    this is test

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let _ = 33355
+        let _ = 3335567
     }
 
     override func didReceiveMemoryWarning() {
